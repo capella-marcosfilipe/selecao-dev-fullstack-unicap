c44c16e8aa9512e36d82581ef61f1c5d3d145241
Marcos Filipe Capella <marcoscapella@outlook.com>
2025-09-20 10:41:45 -0300

    fix(deploy,render): new attempt

--- backend/start.sh
+++ backend/start.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# Exit immediately if a command exits with a non-zero status.
 set -e
 
-# Run migrations
-# alembic upgrade head
-
-# Start Gunicorn
-python -m gunicorn api.main:app -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:$PORT
+uv run gunicorn api.main:app -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:$PORT
